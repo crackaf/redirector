9a3ef4907552f72d58888a1b34f99254183b0339
GHOST HUNT <50262751+ghostoverflow@users.noreply.github.com>
2021-03-16 00:14:06 +0500

    redirector

--- redirector.js
+++ redirector.js
@@ -1,19 +1,26 @@
 
-path = window.location.search //get current url parameters
-path.replace("?", ""); //removing prefix
-document.write("Redirect requested for ", path);
-fetch('https://ghostoverflow.github.io/redirector/data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        if (path in data) {
-            document.getElementById("redirectDetail").innerHTML
-                = "You will be redirected to " + data[path] + " in 5 seconds.";
-            setTimeout(() => window.location.href = data[path], 5000);
-        }
-        else {
-            document.getElementById("redirectDetail").innerHTML
-                = "Could'nt find the specific adrress. Please contact at https://github.com/ghostoverflow/redirector";
-        }
-    })
-    .catch(error => console.error('Error:', error));
+path = window.location.search.replace('?', ''); //get current url parameters
+
+if (path.length == 0) {
+    document.getElementById("redirectDetail").innerHTML
+        = "You will be redirected to " + window.location.href + "/README.md in 5 seconds.";
+    setTimeout(() => window.location.href = 'README.md', 5000);
+}
+else {
+    document.write("Redirect requested for ", path);
+    fetch('https://ghostoverflow.github.io/redirector/data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            if (path in data) {
+                document.getElementById("redirectDetail").innerHTML
+                    = "You will be redirected to " + data[path] + " in 5 seconds.";
+                setTimeout(() => window.location.href = data[path], 5000);
+            }
+            else {
+                document.getElementById("redirectDetail").innerHTML
+                    = 'Could\'nt find the specific adrress. Please contact at <a href="https://github.com/ghostoverflow/redirector">Github Repo</a>.';
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
