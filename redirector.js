5753510a1d7b8b5c83a47187d49873eee328423b
GHOST HUNT <50262751+ghostoverflow@users.noreply.github.com>
2021-03-15 23:53:10 +0500

    redirector

--- redirector.js
+++ redirector.js
@@ -1,19 +1,19 @@
 
 path = window.location.pathname //get current url
 path.replace("/redirector/", ""); //removing prefix
-
+document.write("Redirect requested for ", path);
 fetch('https://ghostoverflow.github.io/redirector/data.json')
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        //document.getElementById("redirectDetail").innerHTML = result;
         if (path in data) {
-            document.getElementById("redirectDetail").innerHTML = "You will be redirected to " + data[path] + " in 5 seconds.";
+            document.getElementById("redirectDetail").innerHTML
+                = "You will be redirected to " + data[path] + " in 5 seconds.";
             setTimeout(() => window.location.href = data[path], 5000);
-
         }
         else {
-
+            document.getElementById("redirectDetail").innerHTML
+                = "Could'nt find the specific adrress. Please contact at https://github.com/ghostoverflow/redirector";
         }
     })
     .catch(error => console.error('Error:', error));
