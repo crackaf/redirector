e99b50e8fe5d3a8d5957a300c0b41a3cda38f359
ghostdart <50262751+ghostdart@users.noreply.github.com>
2021-03-26 17:40:51 +0500

    Updated

--- redirector.js
+++ redirector.js
@@ -1,50 +1,51 @@
 /*******************************************************
- * redirector.js
- * javascript
- * March 2021
+ * @file redirector.js
+ * @language javascript
+ * @date March 2021
  * 
- * This scripts fectch the data.json file
+ * @summary This scripts fectch the data.json file
  * and grabs the url parameter (something after # or ?)
  * crosscheks the parameter with data.json and if found
  * redirects to the specific address
  * 
  * @author Hunzlah Malik @ghostdart
+ * @async
 *********************************************************/
 
-path = window.location.search.replace('?', '').replace('#', '').toLowerCase(); //get current url parameters
+// get current url parameters
+path = window.location.search.replace('?', '').replace('#', '').toLowerCase();
 
-if (path.length == 0) {
-    var timeleft = 3;
+if (path.length == 0) { // if there is no parameter
+    var timeleft = 3; // countdown
     var redirectTimer = setInterval(function () {
-        if (timeleft <= 0) {
+        if (timeleft <= 0) { // countdown is 0
             clearInterval(redirectTimer);
-            window.location.href = 'README.md'
+            window.location.href = 'README.md' // redirect to README file
         }
-        document.getElementById("redirectDetail").innerHTML = "You will be redirected to " + window.location.href + "/README.md in " + timeleft + " seconds.";;
+        document.getElementById("redirectDetail").innerHTML = "You will be redirected to " + window.location.href + "/README.md in " + timeleft + " seconds."; // show countdown and link
         timeleft -= 1;
     }, 1000);
 }
-else {
+else { // there is parameter
     document.write("Redirect requested for ", path);
-    fetch('https://raw.githubusercontent.com/ghostoverflow/redirector/main/data.json')
+    fetch('https://raw.githubusercontent.com/ghostdart/redirector/main/data.json') // fetch the parameter->link file
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            if (path in data) {
-
-                var timeleft = 3;
+            if (path in data) { // parameter is found in data.json
+                var timeleft = 3; // countdown
                 var redirectTimer = setInterval(function () {
-                    if (timeleft <= 0) {
+                    if (timeleft <= 0) { // countdown is 0
                         clearInterval(redirectTimer);
-                        window.location.href = data[path]
+                        window.location.href = data[path] // redirect to specific address
                     }
                     document.getElementById("redirectDetail").innerHTML = 'You will be redirected to <a href="' + data[path] + '">' + data[path] + '</a> in ' + timeleft + ' seconds.';
                     timeleft -= 1;
                 }, 1000);
             }
-            else {
+            else { // parameter not found in data.json
                 document.getElementById("redirectDetail").innerHTML
-                    = 'Could\'nt find the specific adrress. Please contact at <a href="https://github.com/ghostoverflow/redirector/">Github Repo</a>.';
+                    = 'Could\'nt find the specific adrress. Please contact at <a href="https://github.com/ghostdart/redirector/">Github Repo</a>.';
             }
         })
         .catch(error => console.error('Error:', error));
